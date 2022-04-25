21ca1c492b0cf1bb23b426ac33cb23403ab4d2ba
Taito Tanaka <70618595+Taito-Code@users.noreply.github.com>
2022-04-25 23:07:12 +0900

    Study: context basic

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,12 @@
 import * as React from "react";
+import { UserProvider } from "./providers/UserProvider";
 import { Router } from "./router/Router";
 import "./styles.css";
 
 export default function App() {
-  return <Router />;
+  return (
+    <UserProvider>
+      <Router />
+    </UserProvider>
+  );
 }
--- src/components/pages/Top.tsx
+++ src/components/pages/Top.tsx
@@ -1,9 +1,22 @@
 import styled from "styled-components";
+import { SecondaryButton } from "../atoms/button/SecondaryButton";
+import { useHistory } from "react-router-dom";
 
 export const Top = () => {
+  const histry = useHistory();
+
+  const onClickAdmin = () =>
+    histry.push({ pathname: "/users", state: { isAdmin: true } });
+  const onClickGeneral = () =>
+    histry.push({ pathname: "/users", state: { isAdmin: false } });
+
   return (
     <SContainer>
       <h2>TOPページです</h2>
+      <SecondaryButton onClick={onClickAdmin}>管理者ユーザー</SecondaryButton>
+      <br />
+      <br />
+      <SecondaryButton onClick={onClickGeneral}>一般ユーザー</SecondaryButton>
     </SContainer>
   );
 };
